d2a1feb0a55cdc38ac4ab182e06e01f0b3a01076
nxlap97 <nxlap97@gmail.com>
2020-07-11 12:47:54 +0700

    add log entity

---
+++ Smart.Domain/Domain/Log/Logging.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Smart.Core.Domain.Log
+{
+    [Table("Loggings")]
+    public class Logging
+    {
+        [Key]
+        [Required]
+        [StringLength(128)]
+        [Column(TypeName = "VARCHAR")]
+        public string Id { get; set; }
+
+        public string Error { get; set; }
+
+        [StringLength(50)]
+        [Column(TypeName = "VARCHAR")]
+        public string Entity { get; set; }
+    }
+}
